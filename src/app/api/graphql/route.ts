d99b6f0356bf34d08e1a040b5b6d3cefc71a1dda
Peter V <peter@vavro.me>
2023-10-18 11:51:56 -0500

    Set up Apollo Server

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,16 @@
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { NextRequest } from "next/server";
+import typeDefs from './schema/typeDefs'
+import resolvers from './schema/resolvers'
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+    context: async req => ({ req }),
+});
+
+export { handler as GET, handler as POST };

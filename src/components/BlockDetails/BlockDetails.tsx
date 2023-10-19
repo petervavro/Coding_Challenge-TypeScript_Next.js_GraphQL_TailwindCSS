53a85874195770d52b1c0e434aefe72b08d917ab
Peter V <peter@vavro.me>
2023-10-19 16:37:34 -0500

    Move "BlockDetails" to separate dir

--- src/components/BlockDetails/BlockDetails.tsx
+++ src/components/BlockDetails/BlockDetails.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { gql } from "@apollo/client";
+import { gql } from "@/__generated__/gql";
 import { getClient } from "@/providers/ApolloClient";
 import { QueryGetBlockArgs } from '@/__generated__/graphql';
 
-const GET_BLOCK_QUERY = gql(`
+export const GET_BLOCK_QUERY = gql(`
   query GetBlock($blockHash: String) {
     getBlock(blockHash: $blockHash) {
       hash
